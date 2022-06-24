6bfdbb939f34e324d6e066f67cfff7f278e88a6c
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-06-24 18:08:11 +0300

    remove unused code

--- src/components/lightbox/lightbox.styles.jsx
+++ src/components/lightbox/lightbox.styles.jsx
@@ -62,7 +62,6 @@ export const LightBoxContainer=styled.div`
     left: 0;
     right: 0;
     bottom: 0;
-    background-color: rgba(0, 0, 0,.6);
     opacity: 0;
     overflow-y: scroll;
     ${({currentindex})=>currentindex==-1?`opacity:0;z-index:-1;`:`opacity:1;z-index:10`}
